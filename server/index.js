8b581d266806054937e72fbdc3a65c295f4cc235
ApoorvAryan <apoorvaryan64@gmail.com>
2026-05-18 03:15:00 +0530

    Fix backend CORS for local testing

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5175',
+  origin: ["http://localhost:5173", "http://localhost:3000"],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
